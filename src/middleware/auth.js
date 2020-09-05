6b47445d229da11327717d0b56f36dbc9c6c47e5
Kiet Tran <17110165@student.hcmute.edu.vn>
2020-09-05 12:57:52 +0700

    Update auth.js
    - Fix bug errors token exeption may server 503 not available.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,9 +10,9 @@ const auth = async (req, res, next) => {
 
     const token = header.replace('BEARER ', '');
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
         if (!user) {
